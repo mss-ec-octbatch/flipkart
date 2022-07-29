c7b9c0a212eea00e4c5fb50f24a9b09ad870b9eb
poollaashok <poollaashok@gmail.com>
2022-07-29 19:28:42 +0530

    updated

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+Hello i am updating this line from master branch
 This file contains the deployment script
 updating in dev branch
 This file contains the development script updated manually in GitHub GUI
